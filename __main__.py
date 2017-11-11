977231d99a89889ee1f13f12f321232642d2f1df
neilmarshall <neil.marshall@dunelm.org.uk>
2017-11-11 14:07:13 +0000

    Remove dependency on sys in module __main__

--- __main__.py
+++ __main__.py
@@ -8,9 +8,7 @@ Created on Thu Nov  2 21:31:36 2017
 @author: neilmarshall
 """
 
-import sys
-
 from book_log.add_record import add_record
 
 if __name__ == "__main__":
-    add_record(sys.argv[1:])
+    add_record()
